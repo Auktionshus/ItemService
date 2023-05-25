2f0dd65fc45ab710cdade44b5b9d827236dd03fd
Michael H Sloth <michael.h.sloth@gmmail.com>
2023-05-25 13:01:45 +0200

    ItemService_Update

--- ItemService/Controllers/ItemServiceController.cs
+++ ItemService/Controllers/ItemServiceController.cs
@@ -86,7 +86,7 @@ namespace ItemService.Controllers
             MongoClient dbClient = new MongoClient(
                 "mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority"
             );
-            var collection = dbClient.GetDatabase("item").GetCollection<Item>("items");
+            var collection = dbClient.GetDatabase("Items").GetCollection<Item>("Item");
             var items = await collection.Find(_ => true).ToListAsync();
             return Ok(items);
         }
@@ -97,7 +97,7 @@ namespace ItemService.Controllers
             MongoClient dbClient = new MongoClient(
                 "mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority"
             );
-            var collection = dbClient.GetDatabase("item").GetCollection<Item>("items");
+            var collection = dbClient.GetDatabase("Items").GetCollection<Item>("Item");
             Item item = await collection.Find(a => a.Id == id).FirstOrDefaultAsync();
 
             if (item == null)
@@ -107,97 +107,5 @@ namespace ItemService.Controllers
             return Ok(item);
         }
 
-        [HttpGet("{id}/listImages")]
-        public IActionResult ListImages(Guid id)
-        {
-            Item item = Items.FirstOrDefault(a => a.Id == id);
-            if (item == null)
-            {
-                return NotFound($"Item with Id {id} not found.");
-            }
-            return Ok(item.ImageHistory);
-        }
-
-        [HttpPost("uploadImage/{id}"), DisableRequestSizeLimit]
-        public async Task<IActionResult> UploadImage(Guid id)
-        {
-            if (!Directory.Exists(_imagePath))
-            {
-                Directory.CreateDirectory(_imagePath);
-            }
-
-            MongoClient dbClient = new MongoClient(
-                "mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority"
-            );
-            var collection = dbClient.GetDatabase("item").GetCollection<Item>("items");
-            var filter = Builders<Item>.Filter.Eq(a => a.Id, id);
-            Item item = await collection.Find(filter).FirstOrDefaultAsync();
-
-            if (item == null)
-            {
-                return NotFound($"Item with Id {id} not found.");
-            }
-
-            if (item.ImageHistory == null)
-            {
-                item.ImageHistory = new List<ImageRecord>();
-            }
-
-            try
-            {
-                foreach (var formFile in Request.Form.Files)
-                {
-                    // Validate file type and size
-                    if (formFile.ContentType != "image/jpeg" && formFile.ContentType != "image/png")
-                    {
-                        return BadRequest(
-                            $"Invalid file type for file {formFile.FileName}. Only JPEG and PNG files are allowed."
-                        );
-                    }
-                    if (formFile.Length > 1048576) // 1MB
-                    {
-                        return BadRequest(
-                            $"File {formFile.FileName} is too large. Maximum file size is 1MB."
-                        );
-                    }
-                    if (formFile.Length > 0)
-                    {
-                        var fileName = "image-" + Guid.NewGuid().ToString() + ".jpg";
-                        var fullPath = _imagePath + Path.DirectorySeparatorChar + fileName;
-
-                        using (var stream = new FileStream(fullPath, FileMode.Create))
-                        {
-                            formFile.CopyTo(stream);
-                        }
-
-                        var imageURI = new Uri(fileName, UriKind.RelativeOrAbsolute);
-                        var imageRecord = new ImageRecord
-                        {
-                            Id = Guid.NewGuid(),
-                            Location = imageURI,
-                            Date = DateTime.UtcNow,
-                            // Add other properties like Description and AddedBy as needed
-                        };
-
-                        item.ImageHistory.Add(imageRecord);
-                        var update = Builders<Item>.Update.Push(
-                            a => a.ImageHistory,
-                            imageRecord
-                        );
-                        await collection.UpdateOneAsync(filter, update);
-                    }
-                    else
-                    {
-                        return BadRequest("Empty file submitted.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-
-            return Ok("Image(s) uploaded successfully.");
-        }
     }
 }
