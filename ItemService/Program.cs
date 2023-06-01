01a447074906f8376d71d3034e4f0a1c960110a6
Braunerr <mathn519@gmail.com>
2023-06-01 09:44:44 +0200

    vault

--- ItemService/Program.cs
+++ ItemService/Program.cs
@@ -4,23 +4,75 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using NLog;
 using NLog.Web;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 logger.Debug("init main");
 
 try
 {
+    var EndPoint = "https://vault_dev:8201/";
+    logger.Info($"EndPoint: {EndPoint}");
+    var httpClientHandler = new HttpClientHandler();
+    httpClientHandler.ServerCertificateCustomValidationCallback = (
+        message,
+        cert,
+        chain,
+        sslPolicyErrors
+    ) =>
+    {
+        return true;
+    };
+
+    // Initialize one of the several auth methods.
+    IAuthMethodInfo authMethod = new TokenAuthMethodInfo("00000000-0000-0000-0000-000000000000");
+    // Initialize settings. You can also set proxies, custom delegates etc. here.
+    var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+    {
+        Namespace = "",
+        MyHttpClientProviderFunc = handler =>
+            new HttpClient(httpClientHandler) { BaseAddress = new Uri(EndPoint) }
+    };
+    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+    logger.Info($"vault client created: {vaultClient}");
+    // Use client to read a key-value secret.
+    Secret<SecretData> JWTSecrets = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+        path: "JWT",
+        mountPoint: "secret"
+    );
+    Secret<SecretData> MongoSecrets = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+        path: "mongoSecrets",
+        mountPoint: "secret"
+    );
+
+    string? secret = JWTSecrets.Data.Data["Secret"].ToString();
+    string? issuer = JWTSecrets.Data.Data["Issuer"].ToString();
+    string? connectionString = MongoSecrets.Data.Data["ConnectionString"].ToString();
+    logger.Info($"Secret: {secret}");
+    logger.Info($"Issuer: {issuer}");
+    logger.Info($"Connection String: {connectionString}");
+
+    Environment secrets = new Environment
+    {
+        dictionary = new Dictionary<string, string>
+        {
+            { "Secret", secret },
+            { "Issuer", issuer },
+            { "ConnectionString", connectionString }
+        }
+    };
+
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSingleton<Environment>(secrets);
 
     // Add services to the container.
     builder.Services.AddControllers();
 
     var configuration = builder.Configuration;
-    var secret = configuration["Secret"];
-    var issuer = configuration["Issuer"];
-
-    logger.Debug($"Secret: {secret}");
-    logger.Debug($"Issuer: {issuer}");
 
     builder.Services
         .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
